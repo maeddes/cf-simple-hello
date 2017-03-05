e75c6d80344f9edab9821e3adff5112ad1c5534a
Matthias Haeussler <matthias.haeussler@novatec-gmbh.de>
2017-03-05 21:19:50 +0100

    first commit

---
+++ cf-simple-hello/src/main/java/de/maeddes/CfSimpleHelloApplication.java
@@ -0,0 +1,12 @@
+package de.maeddes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfSimpleHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfSimpleHelloApplication.class, args);
+	}
+}
---
+++ cf-simple-hello/src/main/java/de/maeddes/SimpleController.java
@@ -0,0 +1,25 @@
+package de.maeddes;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class SimpleController {
+	
+	@Value("${CF_INSTANCE_INDEX:0}")
+	int instance;
+	
+    @RequestMapping("/")
+    public String index() {
+        return "Hello, bootiful World! Instance: "+instance;
+    }
+    	
+    @RequestMapping("/crash")
+    public String crash() {
+    	System.exit(0); 
+        return "Hello, bootiful World!";
+    }
+
+}
